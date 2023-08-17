7ade9cb56f4b18f9516bbb27b8868fdef20b86d0
fmbaba2904 <femi.oyetola@cloudhight.com>
2023-08-17 00:42:07 -0400

    modify Jenkins, Prometheus and Grafana module

--- module/grafana/main.tf
+++ module/grafana/main.tf
@@ -13,7 +13,7 @@ resource "aws_lb" "grafana-lb" {
 
 resource "aws_lb_target_group" "grafana-tg" {
   name     = var.target_name
-  port     = 30001
+  port     = 31300
   protocol = "HTTP"
   vpc_id   = var.vpc_id
   health_check {
@@ -51,17 +51,17 @@ resource "aws_lb_listener" "grafana-listener2" {
 resource "aws_lb_target_group_attachment" "grafana-attachment1" {
   target_group_arn = aws_lb_target_group.grafana-tg.arn
   target_id        = var.instance1
-  port             = 30001
+  port             = 31300
 }
 
 resource "aws_lb_target_group_attachment" "grafana-attachment2" {
   target_group_arn = aws_lb_target_group.grafana-tg.arn
   target_id        = var.instance2
-  port             = 30001
+  port             = 31300
 }
 
 resource "aws_lb_target_group_attachment" "grafana-attachmen3" {
   target_group_arn = aws_lb_target_group.grafana-tg.arn
   target_id        = var.instance3
-  port             = 30001
+  port             = 31300
 }
